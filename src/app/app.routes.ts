a8dbfbb1bd31a0aeb9c6d47eb91c9f23b5308040
romanvcode <roman.v.ua@gmail.com>
2024-07-29 11:24:31 +0300

    Refactor routing configuration and add NotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
-import { TasksComponent } from './tasks/tasks.component';
+import { routes as userRoutes } from './users/users.routes';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -13,15 +13,10 @@ export const routes: Routes = [
   {
     path: 'users/:userId', // <your-domain>/users/1
     component: UserTasksComponent,
-    children: [
-      {
-        path: 'tasks', // <your-domain>/users/1/tasks
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes,
+  },
+  {
+    path: '**', // <your-domain>/anything-else
+    component: NotFoundComponent,
   },
 ];
